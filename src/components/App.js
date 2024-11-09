20f53fed4c5d1bf885cf7e5919260d48c5e8a7ba
Emily M <emilymwendwa390@gmail.com>
2024-11-09 19:59:12 +0300

    made changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState, useEffect }from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
 function App() {
+  const [plants, setPlants] = useState([]);
+
+  useEffect(() => {
+  fetch('http://localhost:6001/plants')
+  .then(response => response.json())
+      .then(data => setPlants(data))
+      .catch(error => console.error("Error fetching plants:", error));
+  }, []);
   return (
     <div className="app">
-      <Header />
+      <Header Plantsy/>
       <PlantPage />
+      {plants.map(plant => (
+          <li key={plant.id}>
+            <img src={plant.image} alt={plant.name} />
+            <p>{plant.name}</p>
+            <p>${plant.price}</p>
+          </li>
+        ))}
     </div>
   );
 }
